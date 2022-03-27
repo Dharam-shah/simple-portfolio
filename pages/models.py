7b1ed17e6d961adc0cfe421d3dd1c02a7d6ab3ed
Dharam-shah <dharmendra11250@gmail.com>
2022-03-27 09:55:52 +0545

    added media folder

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -5,6 +6,7 @@ class Homepage(models.Model):
     title = models.CharField(max_length=200)
     para1 = models.TextField()
     para2 = models.TextField()
+    image = models.ImageField(upload_to='images/', default='images/profile-photo.png')
     skills_list = models.TextField()
     software_list = models.TextField()
     mail = models.EmailField()
@@ -17,8 +19,9 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    # image = models.ImageField(upload_to='images/')
-    image_path = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/', default='images/html.png')
+    #image = models.ImageField(upload_to='images/')
+    #image_path = models.CharField(max_length=200)
     btntext = models.CharField(max_length=200)
     paragraph = models.TextField()
 
