83bc7dafe1ccb6970f65f973e36da73482412a59
Dharam-shah <dharmendra11250@gmail.com>
2022-03-28 14:27:53 +0545

    updating blog detail page

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<img src="/static/Assignment-img/model-img.png">
 <img src="/static/Assignment-img/database.png">
 <img src="/static/Assignment-img/models.png">
 <img src="/static/Assignment-img/admin.png">
--- pages/views.py
+++ pages/views.py
@@ -30,42 +30,11 @@ def blog(request):
     return render(request, "blogs/blog.html", context)
 
 def blog_detail(request, id):
-    blog_list = [
-        {
-            'id':1,
-            'title':'HTML',
-            'subtitle':'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.',
-            'date':'15 Feb 2022',
-            'image':'img/html.png',
-            'btntext':'Read more about this blog &#8599;',
-            'paragraph':'HTML (HyperText Markup Language) is the most basic building block of the Web. It defines the meaning and structure of web content. Other technologies besides HTML are generally used to describe a web pages appearance/presentatio (CSS) or functionality/behavior (JavaScript). <br> <br> Hypertext refers to links that connect web pages to one another, either within a single website or between websites. Links are a fundamental aspect of the Web. By uploading content to the Internet and linking it to pages created by other people, you become an active participant in the World Wide Web.'
-        },
-        {
-            'id':2,
-            'title':'CSS',
-            'subtitle':'Cascading Style Sheets, or CSS, is a style sheet language used for describing the presentation of a document written in a markup language like HTML.',
-            'date':'15 Feb 2022',
-            'image':'img/css.jpg',
-            'btntext':'Read more about this blog &#8599;',
-            'paragraph':'Cascading Style Sheets, or CSS, is a style sheet language used for describing the presentation of a document written in a markup language like HTML. <br> <br> Cascading Style Sheets, or CSS, is a style sheet language used for describing the presentation of a document written in a markup language like HTML.'
-        },
-        {
-            'id':3,
-            'title':'Python',
-            'subtitle':'Python is a widely used high-level, general-purpose, interpreted, dynamic programming language. Its design philosophy emphasizes code readability, and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java.',
-            'date':'15 Feb 2022',
-            'image':'img/python-django.jpg',
-            'btntext':'Read more about this blog &#8599;',
-            'paragraph':'Python is a widely used high-level, general-purpose, interpreted, dynamic programming language. Its design philosophy emphasizes code readability, and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java. <br> <br> Python is a widely used high-level, general-purpose, interpreted, dynamic programming language. Its design philosophy emphasizes code readability, and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java.'
-        }
-    ]
-
-    for blog in blog_list:
-        if blog['id'] == int(id):
-            context = {
-                'blog':blog,
-            }
-
+    blog = Blog.objects.get(id=id)
+    context = {
+        'blog' : blog
+    }
+    
     return render(request, "blogs/blog_detail.html", context)
 
 
