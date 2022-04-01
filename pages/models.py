4eb1ed6417fc817200be1c1a4551cd4a27dda6ee
Dharam-shah <dharmendra11250@gmail.com>
2022-04-01 14:44:17 +0545

    adding class based view and authentication to blog

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,5 @@
-from distutils.command.upload import upload
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Homepage(models.Model):
@@ -15,13 +15,18 @@ class Homepage(models.Model):
         return self.title
 
 class Blog(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE) 
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='images/', default='images/html.png')
+    #image_path = models.CharField(max_length=200)
     #btntext = models.CharField(max_length=200)
     paragraph = models.TextField()
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog_detail', args=[str(self.id)])
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from pages.models import Blog, Homepage
 from .forms import BlogForm, BlogModelForm
+from django.urls import reverse_lazy
 
+#Class Based Views (Base View)
+from django.views import View
+from django.views.generic import TemplateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+
+#Class Based Views (Generic View)
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 # Create your views here.
 
 def index(request):
@@ -38,7 +49,7 @@ def blog_detail(request, id):
     
     return render(request, "blogs/blog_detail.html", context)
 
-# create new blog
+#create new blog
 def blog_create(request):
     if request.method == 'POST':
         form = BlogModelForm(request.POST,request.FILES)
@@ -55,7 +66,7 @@ def blog_create(request):
 
     return render(request, 'blogs/blog_create.html', {'form': form})
 
-# update blog
+#update blog
 def blog_update(request,id):
     blog = Blog.objects.get(id=id)
     if request.method == 'POST':
@@ -78,3 +89,96 @@ def blog_delete(request,id):
     blog = Blog.objects.get(id=id)
     blog.delete()
     return redirect('/blog')
+
+
+
+
+#1
+class Index(TemplateView):
+    template_name = 'pages/index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        homepage_data = Homepage.objects.get(id=1)
+        context = {
+        'title':homepage_data.title,
+        'para1':homepage_data.para1,
+        'para2':homepage_data.para2,
+        'skills':homepage_data.skills_list,
+        'software':homepage_data.software_list,
+        'mail':homepage_data.mail,
+        'image':homepage_data.image,
+        }
+        return context
+
+#Class Based Views (Generic View)
+#3
+class BlogList(ListView):
+    model = Blog
+    template_name=  'blogs/blog.html'
+    context_object_name = 'blogs_list'
+
+#5 
+class BlogDetail(DetailView):
+    model = Blog
+    template_name=  'blogs/blog_detail.html'
+    context_object_name = 'blog'
+
+
+class MyBlogDetail(LoginRequiredMixin,UserPassesTestMixin,DetailView):
+    login_url = '/signin/'
+    model = Blog
+    template_name=  'blogs/myblog_detail.html'
+    context_object_name = 'blog'
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user     
+
+#7
+class BlogCreate(CreateView):
+    model = Blog
+    form_class = BlogModelForm
+    template_name = 'blogs/blog_create.html'
+    success_url = '/blog'
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+#9
+class BlogUpdate(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    login_url = '/signin/'
+    model = Blog
+    #form_class = BlogModelForm
+    fields = ['title','subtitle', 'paragraph','image'] 
+    template_name = 'blogs/blog_update.html'
+    #11 changes to model to get absolute url
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user     
+
+#12
+class BlogDelete(LoginRequiredMixin,UserPassesTestMixin,DeleteView): 
+    login_url = '/signin/'
+    model = Blog
+    template_name = 'blogs/blog_delete.html'
+    success_url = reverse_lazy('blog')   
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user  
+
+
+
+
+class MyBlogList(LoginRequiredMixin,ListView):
+    login_url = '/signin/'
+    model = Blog
+    template_name=  'blogs/myblogs.html'
+    context_object_name = 'blogs_list'
+
+    def get_queryset(self):
+        user = self.request.user
+        return Blog.objects.filter(author=user)
+
+
