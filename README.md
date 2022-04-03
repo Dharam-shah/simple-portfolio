d54315a7bd46aa8b828a7765d2bab9a4f9449e36
Dharam-shah <dharmendra11250@gmail.com>
2022-04-03 08:00:23 +0545

    implemented postgres sql

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-<img src="/static/Assignment-img/register.png">
-<img src="/static/Assignment-img/signin.png">
-<img src="/static/Assignment-img/profile.png">
+#Table using Postgres
+<img src="/static/Assignment-img/table.png">
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,13 +78,25 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',  #database name
+        'USER': 'postgres', #database user
+        'PASSWORD': 'Nature11250', #database password
+        'HOST': 'localhost', #database host
+        'PORT': '5432', #database port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
