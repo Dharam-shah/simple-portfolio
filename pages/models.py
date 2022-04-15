0abebf857e20c086fe8a2eacaeea50203b3d1744
Dharam-shah <dharmendra11250@gmail.com>
2022-04-15 17:56:36 +0545

    updating models.py

--- pages/models.py
+++ pages/models.py
@@ -9,14 +9,16 @@ class HomepageBanner(models.Model):
     banner_image = models.ImageField(upload_to='images/')
 
     def __str__(self):
-        return f'{slef.id} | {self.banner_title}'
+        return f'{self.id} | {self.banner_title}'
 
 """ Homepage Blogs,Article page Blog and Blog detail page blog Model """
 class Blog(models.Model):
-    #blog_image = models.ImageField(upload_to='images/')
+    blog_image = models.ImageField(upload_to='images/')
     blog_title = models.CharField(max_length=250)
     blog_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
+    slug = models.SlugField(unique=True, max_length=255, default=blog_title)
+    published = models.BooleanField(default=True)
 
     def __str__(self):
         return f'ID{self.id} | {self.blog_title}'
